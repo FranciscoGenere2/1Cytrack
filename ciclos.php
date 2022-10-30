8c8bc3814455c53ae85caa77b4a01ab431d6e742
Francisco Genere <fran_frandije@hotmail.com>
2022-10-30 00:56:17 -0400

    Ciclos

---
+++ ciclos.php
@@ -0,0 +1,59 @@
+<?php include('./constant/layout/head.php'); ?>
+<?php include('./constant/layout/header.php'); ?>
+
+<?php include('./constant/layout/sidebar.php'); ?>
+
+<?php include('./constant/connect');
+
+
+?>
+<div class="page-wrapper">
+
+    <div class="row page-titles">
+        <div class="col-md-5 align-self-center">
+            <h3 class="text-primary"> Ciclos</h3>
+        </div>
+        <div class="col-md-7 align-self-center">
+            <ol class="breadcrumb">
+                <li class="breadcrumb-item"><a href="javascript:void(0)">Inicio</a></li>
+                <li class="breadcrumb-item active">Ciclos</li>
+            </ol>
+        </div>
+    </div>
+
+
+    <div class="container-fluid">
+
+
+
+<link rel="stylesheet" type="text/css" href="custom\css\custom.css">
+        <div class="card">
+            <div class="card-body">
+
+                <a href="#"><button class="btn btn-primary">Añadir Período</button></a>
+
+    
+                <div class="table-responsive m-t-40">
+                
+                <div class="bloqueperiodo">
+
+                <p>Valor Período</p>
+                <ul>
+                    <li>
+                      --  Duracion del período ----------------------<p style="color: red; display: inline;">\Dia</p>
+                    </li>
+                    <li>
+                      --  Duracion del ciclo ----------------------<p style="color: red; display: inline;">\Dia</p>
+                    </li>
+                </ul>
+                </div>
+
+
+                </div>
+                <head>  
+ 
+</head>
+              
+
+
+        <?php include('./constant/layout/footer.php'); ?>
